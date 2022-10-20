01bea88202fe206fad6224a640e4344c52d1e81f
Andressa Arcocha <andressaarcocha@hotmail.com>
2022-10-20 10:35:31 -0300

    se agrego footer a error

--- index.js
+++ index.js
@@ -7,3 +7,37 @@ const users = [ ];
 
 users.push(useradmin);
 localStorage.setItem('users', JSON.stringify(users));
+
+var swiper = new Swiper('.swiper-container', {
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev'
+	},
+	slidesPerView: 1,
+	spaceBetween: 10,
+	// init: false,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+
+  
+	breakpoints: {
+	  620: {
+		slidesPerView: 1,
+		spaceBetween: 20,
+	  },
+	  680: {
+		slidesPerView: 2,
+		spaceBetween: 40,
+	  },
+	  920: {
+		slidesPerView: 3,
+		spaceBetween: 40,
+	  },
+	  1240: {
+		slidesPerView: 4,
+		spaceBetween: 50,
+	  },
+	} 
+    });
