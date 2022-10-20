1cfa3eef79039e2ed34ad692b23e203b04eba551
Enzo Lobo <enzolobo@gmail.com>
2022-10-20 17:19:33 -0300

    comento lineas q generan conflicto

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const useradmin = {
-    email : 'admin@admin.com',
-    password : 'admin',
-    role : 'admin'
-}
-const users = [ ];
+// const useradmin = {
+//     email : 'admin@admin.com',
+//     password : 'admin',
+//     role : 'admin'
+// }
+// const users = [ ];
 
-users.push(useradmin);
-localStorage.setItem('users', JSON.stringify(users));
+// users.push(useradmin);
+// localStorage.setItem('users', JSON.stringify(users));
 
 var swiper = new Swiper('.swiper-container', {
 	navigation: {
